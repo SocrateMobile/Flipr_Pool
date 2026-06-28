6dd2a375ef482046d2d6af32544d9cd167341538
socrate <Torjku11APL&>
2026-06-28 17:41:12 +0200

    🐛 Correction du libellé de l'entité Version et v3.3.2

--- custom_components/flipr_pool/const.py
+++ custom_components/flipr_pool/const.py
@@ -1,7 +1,7 @@
 """Constants for the Flipr Pool integration."""
 
 DOMAIN = "flipr_pool"
-VERSION = "3.3.1"
+VERSION = "3.3.2"
 API_BASE_URL = "https://apis.goflipr.com"
 
 # ── Configuration de traitement ────────────────────────────
